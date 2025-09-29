0b7ecd9f78f1dd9ded4b8ee7d8ba9b4868361d91
artanvrajolli <artanvrajolli@gmail.com>
2025-09-29 16:27:35 +0200

    Export app for Vercel serverless deployment

--- app.js
+++ app.js
@@ -122,8 +122,12 @@ app.use('/:path(*)', upload.any(), async function (req, res) {
 
 
 
-app.listen(port, function () {
-    console.log("Server is running on port " + port);
-});
+export default app;
+
+if (!process.env.VERCEL) {
+    app.listen(port, function () {
+        console.log("Server is running on port " + port);
+    });
+}
 
 
